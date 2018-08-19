efd3ab824906b6a5d5bf7240b40813b9069036c5
KaranGaur <karan.gopal.gaur@gmail.com>
2018-08-19 15:45:03 +0530

    FIRST CHECKPOINT FINAL

--- db/mongoose.js
+++ db/mongoose.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/MinorProject') , { useNewUrlParser: true };
+mongoose.connect('mongodb://localhost:27017/MinorProject' , { useNewUrlParser: true });
 
 module.exports = {
     mongoose
